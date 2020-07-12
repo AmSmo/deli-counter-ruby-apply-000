b75cae6dca1fe9599487e74b9ba227fc70591b99
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-12 14:43:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,19 +6,29 @@ def line(store)
     puts "The line is currently empty."
   else
     line_string = ""
-    count = 1
+    # count = 1
     store.each do |customer|
-      line_string +=" #{count}. #{customer}"
-      count += 1
+      line_string +=" #{store.find_index(customer) + 1}. #{customer}" 
+      #find_index may not work if two customers named the same, so count was the original plan
+      # count += 1
     end
     puts "The line is currently:#{line_string}"
   end
 end
 
 # adds to the line and gives name, also adjusts from 0 indexing
-def take_a_number(store, name)
-  store << name
-  puts "Welcome, #{name}. You are number #{store.find_index(name) + 1} in line."
+# def take_a_number(store, name)
+#   store << name
+#   puts "Welcome, #{name}. You are number #{store.size} in line."
+#   #again find_index is problematic if two customers have the same name, another way to do this would be to do size
+# end
+
+
+count= 0
+def take_a_number(store)
+    count += 1
+    store.push(count)
+    puts "Currently serving #{store[-1]}"
 end
 
 # takes out first person in list and serves them
